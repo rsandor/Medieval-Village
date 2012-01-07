6f241129e03d227169365067f7156b187c4478f9
Ryan Sandor Richards <sandor.richards@gmail.com>
2012-01-06 23:52:15 -0800

    Added an images directory and a test image.

--- js/mv.js
+++ js/mv.js
@@ -1,3 +1,11 @@
 (function() {
-
+  var MedievalVillage;
+  MedievalVillage = (function() {
+    function MedievalVillage() {}
+    MedievalVillage.start = function() {
+      return console.log("MedievalVillage.start!");
+    };
+    return MedievalVillage;
+  })();
+  $(MedievalVillage.start);
 }).call(this);
--- mv.coffee
+++ mv.coffee
@@ -3,4 +3,14 @@
 # A game by Thomas O'Neil and Ryan Richards
 #
 
+class MedievalVillage
+	
+	
+	# Initializes the game and starts the main game loop
+	@start: ->
+		console.log "MedievalVillage.start!"
+		
 
+
+# Start the game!
+$ MedievalVillage.start
